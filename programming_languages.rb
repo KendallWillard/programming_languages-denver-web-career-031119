0cc41d85b0d767491f8e1fa8cb50f70382f6a360
github username <github email address>
2019-03-08 22:19:05 -0700

    Done.

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,3 +1,31 @@
+require 'pry'
+
+
 def reformat_languages(languages)
   # your code here
+
+  #Format the new hash with everything but the programming styles
+  new_hash = {}
+  languages.each do |style, style_data|
+    style_data.each do |language, language_data|
+      #Store all the info except programming styles in hash
+      new_hash[language] = language_data
+      #Add the new key/value to hold the programming styles
+      new_hash[language][:style] = []
+    end
+  end
+  #binding.pry
+
+  #Add the programming styles to the hash
+  languages.each do |style, style_data|
+    style_data.each do |language, language_data|
+      if style == :oo
+        new_hash[language][:style] << :oo
+      else
+        new_hash[language][:style] << :functional
+
+      end
+    end
+  end
+  return new_hash
 end
